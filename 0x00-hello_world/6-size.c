9bcdbc07a0146c033c683747358e114aeea69e22
sohataher <sohataher50@gmail.com>
2023-07-17 06:44:42 +0300

    add file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Description: using sizeof to print the size of various types.
  *
  * Return: Always 0 (success)
- */
+*/
 
 int main(void)
 {
