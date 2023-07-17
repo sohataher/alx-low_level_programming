fdcb40a473f8bcfadf505139dc255841dbb44829
sohataher <sohataher50@gmail.com>
2023-07-17 06:34:06 +0300

    add file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,9 @@
 
 /**
  *
- * main -Entry point
+ * main - Entry point
  *
- * description: using sizeof to print the size of various types.
+ * Description: using sizeof to print the size of various types.
  *
  * Return: Always 0 (success)
 */
@@ -12,7 +12,7 @@
 int main(void)
 {
 	printf("Size of a char: %u byte(s)\n", sizeof(char));
-	printf("Size of a int: %u byte(s)\n", sizeof(int));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
 	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %u byte(s)\n", sizeof(float));
