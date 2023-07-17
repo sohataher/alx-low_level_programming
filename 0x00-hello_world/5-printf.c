e2cddfe6af211b62ff028f7a0e95d8dc2bcc31ed
sohataher <sohataher50@gmail.com>
2023-07-17 05:48:20 +0300

    add file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: A C program that prints with printf function
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+        printf("\"Programming is like building a multilingual puzzle\n");
+        return (0);
+}
