13612898cde200d3822ce44fa44a106e9c23b4c1
sohataher <sohataher50@gmail.com>
2023-07-18 03:42:01 +0300

    add file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: play the alphabet game
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+			ch++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
