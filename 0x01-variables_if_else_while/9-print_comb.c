e962cad809030f77ad0f3d59ed5591556fba3749
sohataher <sohataher50@gmail.com>
2023-07-18 04:25:27 +0300

    add file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: play the alphabet game
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 57)
+	{
+		putchar(digit);
+		if (digit != 57)
+			putchar(',');
+			putchar(' ');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
