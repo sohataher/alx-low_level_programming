791e9f9c1ffa6ed9709ec8a3cd85ec95902e8e5a
sohataher <sohataher50@gmail.com>
2023-07-19 02:28:39 +0300

    add file

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,10 +2,12 @@
 #include "main.h"
 
 /**
- * _putchar: write the character to stdout
+ * _putchar - write the character to stdout
+ * @c: printed character
+ * Return: 1 (success)
  */
 
-int putchar(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
