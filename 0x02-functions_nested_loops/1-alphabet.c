639e0fc474b502a44060ca00578bd4f43b87c792
sohataher <sohataher50@gmail.com>
2023-07-19 02:41:15 +0300

    add file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphapet using _putchar
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
