f5ded70e5369c91298458ac095c35b29343f99f1
sohataher <sohataher50@gmail.com>
2023-07-18 07:04:39 +0300

    add file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
