1d7a6a93cde529c5a376c0ebc98a8fa51e15b539
sohataher <sohataher50@gmail.com>
2023-07-21 02:39:17 +0300

    add file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,11 @@ void more_numbers(void)
 		for (i = 0; i < 15; i++)
 		{
 			if (i < 10)
-				_putchar(i);
+				_putchar(i + 48);
 			else
 			{
-				_putchar(i / 10);
-				_putchar(i % 10);
+				_putchar(i / 10 + 48);
+				_putchar(i % 10 + 48);
 			}
 		}
 		_putchar('\n');
