3e1e63522ebc2f520b96dd1505794ad3926f9d3e
sohataher <sohataher50@gmail.com>
2023-07-21 02:28:54 +0300

    add file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,12 +6,17 @@
 
 void more_numbers(void)
 {
-	int num, i;
+	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 15; i++)
 	{
-		for (num = '0'; num <= '14'; num++)
-			_putchar(num);
-		_putchar('\n');
+		if (i < 10)
+			_putchar('i');
+		else
+		{
+			_putchar('i / 10');
+			_putchar('i % 10');
+		}
 	}
+	_putchar('\n');
 }
