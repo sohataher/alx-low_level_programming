84ae028815825a123634688505675a44a5a38bf4
sohataher <sohataher50@gmail.com>
2023-07-21 04:31:31 +0300

    add file

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  print_square- draws a straight line
+ *
+ * @size: input of the function
+*/
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
+	}
+}
