7bb893880c00daef173c0ced84e9fce27a1cf015
sohataher <sohataher50@gmail.com>
2023-07-21 04:20:36 +0300

    add file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ *
+ * @n: input of the function
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+}
