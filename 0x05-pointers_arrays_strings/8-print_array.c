58d99645fbad5aaf6a567f3660a0e2349eb9a02d
sohataher <sohataher50@gmail.com>
2023-07-26 02:53:51 +0300

    add file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  print_array - prints n elements of an array of integers
+ *
+ * @a: string parameter
+ * @n: elements parameter input
+ *
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		if (i != (n - 1))
+			printf(', ');
+	}
+	_putchar('\n');
+}
