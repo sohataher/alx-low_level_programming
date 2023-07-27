b9ca9ccfb127c22a4deda46c3e27992a8329ed3a
sohataher <sohataher50@gmail.com>
2023-07-27 23:06:03 +0300

    add file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (*s1 != *s2)
 		{
-			equal = ((int)*s1 - 48) - ((int)*s2 - 48));
+			equal = ((int)(*s1) - 48) - ((int)(*s2) - 48));
 			break;
 		}
 		s1++;
