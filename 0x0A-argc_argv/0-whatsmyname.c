4687c0724165b0892b21c138b1fd50af116b0c4e
sohataher <sohataher50@gmail.com>
2023-09-07 02:01:12 +0300

    add file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main - prints the program name, followed by a new line.
+ *
+ * @argc: int
+ * @argv: array
+ *
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
