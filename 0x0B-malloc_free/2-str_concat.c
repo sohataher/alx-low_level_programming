b17b50626144fe50cde2755d6bc26680db90468d
sohataher <sohataher50@gmail.com>
2023-09-11 07:26:19 +0300

    add file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,24 +24,24 @@ int _len(char *s)
 
 char *str_concat(char *sl, char *s2)
 {
-	int sizel, size2, i;
+	int size1, size2, i;
 	char *con;
 
-	if (sl == NULL)
+	if (s1 == NULL)
 		s1 = "\0";
 	if (s2 == NULL)
 		s2 = "\0";
 
 	sizel = _len(s1);
 	size2 = _len(s2);
-	con = malloc((sizel + size2) * sizeof(char) + 1);
+	con = malloc((size1 + size2) * sizeof(char) + 1);
 
 	if (con == 0)
 		return (0);
 
-	for (i = 0; i <= sizel + size2; i++)
+	for (i = 0; i <= size1 + size2; i++)
 	{
-		if (i < sizel)
+		if (i < size1)
 			con[i] = s1[i];
 		else
 			con[i] = s2[i - size1];
